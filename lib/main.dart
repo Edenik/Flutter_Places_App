ec5ccfa65d44db9c7920a422bc680da5b326912d
Edenik <Edenik5@gmail.com>
2020-10-06 12:46:23 +0300

    adding screens + providers + models

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_places_app/screens/add_place_screen.dart';
+import 'package:flutter_places_app/screens/places_list_screen.dart';
+import 'package:provider/provider.dart';
+import './providers/user_places.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,14 +12,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Places',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        accentColor: Colors.amber,
+    return ChangeNotifierProvider.value(
+      value: UserPlaces(),
+      child: MaterialApp(
+        title: 'Flutter Places',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.amber,
+        ),
+        home: PlacesListScreen(),
+        routes: {
+          AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
+        },
       ),
-      // home: ,
     );
   }
 }
